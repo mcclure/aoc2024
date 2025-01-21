f7a2a2a24344afac1de02eddedddffaceec58ff5
mcc <andi.m.mcclure@gmail.com>
2025-01-21 14:14:25 -0500

    NON-WORKING Number-echo

---
+++ 03-01-multiply/src/number-echo.s
@@ -0,0 +1,39 @@
+# Writes the number 69 to the console using only calls. Runs on 64-bit Linux only.
+
+    .global main
+
+    # GNU requires this. Pathetic
+    .section .note.GNU-stack,"",@progbits
+
+    .data
+
+printf_arg: .asciz "%d\n"
+
+input: .asciz "\
+137\
+"
+
+    .text
+
+_exit:
+    # exit(0)
+
+    mov     $60, %rax               # system call 60 is exit
+    xor     %rdi, %rdi              # we want return code 0
+    syscall                         # invoke operating system to exit
+
+_print: # ARGUMENTS: %esi
+    # Modeled on https://stackoverflow.com/a/38335743
+    # printf( "%d\n", [%esi] )
+    
+    push %rbx
+    lea  printf_arg(%rip), %rdi
+    xor %eax, %eax          # "Zeroing EAX is efficient way to clear AL." (?)
+    call printf
+    pop %rbx
+
+    jmp _exit
+
+main:
+    mov  $69, %esi           # "Writing to ESI zero extends to RSI." (?)
+    jmp _print
