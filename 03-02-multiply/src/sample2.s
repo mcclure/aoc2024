666d7ec77669d943ffb8bc0bc08be4085f7a9bbd
mcc <andi.m.mcclure@gmail.com>
2025-01-21 22:35:21 -0500

    03-02 works

--- 03-02-multiply/src/sample2.s
+++ 03-02-multiply/src/sample2.s
@@ -11,7 +11,7 @@
 
 printf_arg: .asciz "%ld\n"          # See test/prid.c
 
-# !!!!!!!!!!!!!!!! This line will be replaced by "input"
+input: .asciz "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
 
 match_prefix:  .ascii "mul("
 match_prefix2: .ascii "don't()"
