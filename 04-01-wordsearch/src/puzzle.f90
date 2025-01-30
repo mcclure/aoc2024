b31be91adb5a0721f97e2ba8f145da4f36129753
mcc <andi.m.mcclure@gmail.com>
2025-01-30 16:12:58 -0500

    Spelling errors in comment

--- 04-01-wordsearch/src/puzzle.f90
+++ 04-01-wordsearch/src/puzzle.f90
@@ -17,8 +17,8 @@ program puzzle
 
     ! Although F2023 (see https://wg5-fortran.org/N2201-N2250/N2212.pdf section 2.2)
     ! supports a deferred-length string being length-initialized by get_command_argument,
-    ! GNU FORTRAN as of 14.2.0 appears to not do this and the path is written out as length 0.
-    ! Therefore, call twice, once to get the path output, and then after allocating cal again
+    ! GNU FORTRAN as of 14.2.0 appears to not do this and the path out is an empty string.
+    ! Therefore, call twice, once to get the path length, and then after allocating call again
     ! to get the path. If called with expected (conformant?) behavior this is harmless.
     call get_command_argument(1, path, path_length)
     print *, path_length, path
