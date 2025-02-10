b17c5164dcaf08a0acf06f6764f1a241aa4c48dd
mcc <andi.m.mcclure@gmail.com>
2025-02-10 12:46:04 -0500

    Total graph

--- 05-01-ordering/src/puzzle.tcl
+++ 05-01-ordering/src/puzzle.tcl
@@ -10,8 +10,8 @@ proc is_empty {s} {
 # Final result
 set total 0
 
-# partial: Dict representing a shallow graph (noop)
-# order: Dict representing all paths in graph
+# partial: Dict representing graph edges
+# order: Dict representing all edges reachable from each edge
 
 # Begin
 set input [open [lindex $argv 0] r]
@@ -27,19 +27,44 @@ while 1 {
 }
 
 puts $partial
-dict for {a blist} $partial {
-    foreach b $blist {
-        while 1 {
-            dict lappend order $a $b
-            if {![dict exists $partial $b]} break
-            set b [dict get $partial $b]
+dict for {a queue} $partial {
+##    puts "A $a"
+    # Seen list resets once per root
+    set seen ""
+    # Starting from loop, repeat bfs search until graph exhausted
+    while {[expr {0 < [llength $queue]}]} {
+##        puts "Pass: $queue"
+##        puts "Len: [llength $queue]"
+        # Start building queue for next loop
+        set queue_next ""
+
+        # For each edge we have to check this loop
+        foreach b $queue {
+##            puts "seen: {$seen} check: $b [dict exists $seen $b]"
+            # If edge not already seen for this root
+            if {![dict exists $seen $b]} {
+                # Preserve final result
+                dict lappend order $a $b
+                # Ensure we don't revisit this edge
+                dict set seen $b 1
+                # Queue all edges visible from this edge
+                if [dict exists $partial $b] {
+                    foreach b2 [dict get $partial $b] {
+##                        puts "inner: $b2"
+                        lappend queue_next $b2
+                    }
+                }
+            }
         }
+        # Repeat with new queue
+        set queue $queue_next
     }
+##    puts ""
 }
 puts $order
 puts ""
 
-unset partial a b blist
+unset partial a b b2 seen queue queue_next
 
 # Extract update candidates
 while 1 {
@@ -51,3 +76,5 @@ while 1 {
 
 unset line
 close $input
+
+puts "\n$total"
