6616840a438419494c0e65530e9733cee786702d
mcc <andi.m.mcclure@gmail.com>
2025-02-10 11:38:05 -0500

    Import partial order list, incorrectly expand it one step

--- 05-01-ordering/src/puzzle.tcl
+++ 05-01-ordering/src/puzzle.tcl
@@ -3,6 +3,51 @@ if { $argc != 1 } {
     exit 1
 }
 
+proc is_empty {s} {
+    return [expr {$s == ""}]
+}
+
+# Final result
+set total 0
+
+# partial: Dict representing a shallow graph (noop)
+# order: Dict representing all paths in graph
+
+# Begin
 set input [open [lindex $argv 0] r]
-puts [gets $input]
+
+# Extract ordering
+while 1 {
+    set line [gets $input]
+    if [is_empty $line] break
+
+    lassign [split $line "|"] a b
+
+    dict lappend partial $a $b
+}
+
+puts $partial
+dict for {a blist} $partial {
+    foreach b $blist {
+        while 1 {
+            dict lappend order $a $b
+            if {![dict exists $partial $b]} break
+            set b [dict get $partial $b]
+        }
+    }
+}
+puts $order
+puts ""
+
+unset partial a b blist
+
+# Extract update candidates
+while 1 {
+    set line [gets $input]
+    if [is_empty $line] break
+    set line [split $line ","]
+    puts $line
+}
+
+unset line
 close $input
