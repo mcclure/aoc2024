d97b01b9281617be693166b4466d9188e986af36
mcc <andi.m.mcclure@gmail.com>
2025-05-01 16:03:50 -0400

    Count number of lines

--- 07-01-operations/app/Puzzle.hs
+++ 07-01-operations/app/Puzzle.hs
@@ -1,10 +1,22 @@
 import System.Environment
 import System.IO
 
+takeLineImpl :: Handle -> Int -> IO (Int)
+takeLineImpl inHandle acc = 
+    do inEof <- hIsEOF inHandle
+       if inEof
+           then return (acc)
+           else do inStr <- hGetLine inHandle
+                   let result = 1
+                   takeLineImpl inHandle (acc + result)
+
+takeLine :: Handle -> IO (Int)
+takeLine inHandle = do takeLineImpl inHandle 0
+
 main :: IO ()
 main = do
     [inFile] <- getArgs              -- TODO exceptional cases
     file <- openFile inFile ReadMode
-    line <- hGetLine file            -- TODO do in loop
-    putStr line
+    total <- takeLine file            -- TODO do in loop
+    putStr (show total)
     putStr "\n"
