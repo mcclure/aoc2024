5e10e45f0ab08b748072d251c66d5a0f25c34c72
mcc <andi.m.mcclure@gmail.com>
2025-05-02 14:43:32 -0400

    Better amb test

--- 07-01-operations/app/Ambtest.hs
+++ 07-01-operations/app/Ambtest.hs
@@ -1,10 +1,12 @@
 import Control.Monad.Amb
 
-sumTen :: Amb Bool Bool
-sumTen = do
+sumTen :: Int -> Amb Bool Bool
+sumTen plus = do
     times <- aMemberOf [-1 :: Int, 1 :: Int]
-    return (6 + 4 * times == 10)
+    return (6 + plus * times == 10)
 
 main :: IO ()
 main = do
-    putStr (show (isPossible sumTen))
+    print (isPossible (sumTen 3))
+    print (isPossible (sumTen 4))
+    print (isPossible (sumTen 5))
