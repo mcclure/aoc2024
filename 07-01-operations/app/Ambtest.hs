2112bc28024ab2d938e378d55ceaa5220cbd14b7
mcc <andi.m.mcclure@gmail.com>
2025-05-02 14:25:28 -0400

    Fix int warning

--- 07-01-operations/app/Ambtest.hs
+++ 07-01-operations/app/Ambtest.hs
@@ -2,10 +2,9 @@ import Control.Monad.Amb
 
 sumTen :: Amb Bool Bool
 sumTen = do
-    times <- aMemberOf [-1, 1]
+    times <- aMemberOf [-1 :: Int, 1 :: Int]
     return (6 + 4 * times == 10)
 
--- Read one value from command line, feed it to takeLines, print result
 main :: IO ()
 main = do
     putStr (show (isPossible sumTen))
