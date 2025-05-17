39a9d08080707cb1862b39fe6effd92ce05a766b
mcc <andi.m.mcclure@gmail.com>
2025-05-17 14:51:51 -0400

    Read file from STDIN

--- 09-01-disk/src/puzzle.adb
+++ 09-01-disk/src/puzzle.adb
@@ -1,16 +1,39 @@
 with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Task_Identification; use Ada.Task_Identification;
 with Ada.Command_Line;
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 
 procedure Puzzle is
    package CLI renames Ada.Command_Line;
       F         : File_Type;
+      Line      : String(1..20001); -- Will fail on inputs over 20k chars
+      Last      : Natural;
+      I         : Natural;
+      Mem_Len   : Natural;
+      Temp_Digit : Natural;
+      --Mem       : Array (Natural range <>) of Natural;
 begin
-   if CLI.Argument_Count /= 1 then
-      Put_Line (Standard_Error, "** Expected filename as argument.");
-      CLI.Set_Exit_Status( 1 );
-      Abort_Task (Current_Task);
-   end if;
+   -- if CLI.Argument_Count /= 1 then
+   --    Put_Line (Standard_Error, "** Expected filename as argument.");
+   --    CLI.Set_Exit_Status( 1 );
+   --    Abort_Task (Current_Task);
+   -- end if;
 
-   Put_Line (Item => "Argument Count:" & CLI.Argument_Count'Img);
+   -- Open (F, In_File, CLI.Argument(1));
+   Get_Line(Line, Last);
+   -- if not Done then
+   --    Put_Line (Standard_Error, "** Expected file to be one line long.");
+   --    CLI.Set_Exit_Status( 1 );
+   --    Abort_Task (Current_Task);
+   -- end if;
+   -- Close (F);
+
+   Put_Line (Item => "Line len: " & Last'Image);
+
+   Mem_Len := 0;
+   for I in 1..Last loop
+      Temp_Digit := Character'Pos(Line(I)) - Character'Pos('0');
+      Mem_Len := Mem_Len + Temp_Digit;
+   end loop;
+   Put_Line (Item => "Mem len:  " & Natural'Image( Mem_Len ) );
 end Puzzle;
