b46079ddcb5c45d52af5882e940a593050c7beb9
mcc <andi.m.mcclure@gmail.com>
2025-05-17 15:20:40 -0400

    Initialize variable-sized array

--- 09-01-disk/src/puzzle.adb
+++ 09-01-disk/src/puzzle.adb
@@ -5,13 +5,17 @@ with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 
 procedure Puzzle is
    package CLI renames Ada.Command_Line;
-      F         : File_Type;
-      Line      : String(1..20001); -- Will fail on inputs over 20k chars
-      Last      : Natural;
-      I         : Natural;
-      Mem_Len   : Natural;
-      Temp_Digit : Natural;
-      --Mem       : Array (Natural range <>) of Natural;
+      F           : File_Type;
+      Line        : String(1..20001); -- Will fail on inputs over 20k chars
+      Last        : Natural;
+      I           : Natural;
+      Mem_Len_Tmp : Natural;
+      Temp_Digit  : Natural;
+      procedure Run(Mem_Len : Natural) is
+         Mem       : Array (Natural range 1..Mem_Len) of Integer range -1..Integer'Last;
+      begin
+         Put_Line (Item => "Done");
+      end;
 begin
    -- if CLI.Argument_Count /= 1 then
    --    Put_Line (Standard_Error, "** Expected filename as argument.");
@@ -19,6 +23,7 @@ begin
    --    Abort_Task (Current_Task);
    -- end if;
 
+   -- -- Is there a version of Get_Line that works on a filehandle, I didn't find it :(
    -- Open (F, In_File, CLI.Argument(1));
    Get_Line(Line, Last);
    -- if not Done then
@@ -30,10 +35,13 @@ begin
 
    Put_Line (Item => "Line len: " & Last'Image);
 
-   Mem_Len := 0;
+   Mem_Len_Tmp := 0;
    for I in 1..Last loop
       Temp_Digit := Character'Pos(Line(I)) - Character'Pos('0');
-      Mem_Len := Mem_Len + Temp_Digit;
+      Mem_Len_Tmp := Mem_Len_Tmp + Temp_Digit;
    end loop;
-   Put_Line (Item => "Mem len:  " & Natural'Image( Mem_Len ) );
+   Put_Line (Item => "Mem len:  " & Natural'Image( Mem_Len_Tmp ) );
+
+   -- Start process
+   Run(Mem_Len_Tmp);
 end Puzzle;
